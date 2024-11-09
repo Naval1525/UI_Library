b71dbf646c5abade220a216d79014a4d16db45fd
Naval15 <e23cseu0962@bennett.edu.in>
2024-11-09 21:28:19 +0530

    added return option

--- app/(Dashboard)/layout.js
+++ app/(Dashboard)/layout.js
@@ -1,21 +1,30 @@
-import ComponentLibrary from "./_components/ComponentLibrary";
+"use client"
 
 
-function layout({ children }) {
+function Layout({ children }) {
+  const handleBackClick = () => {
+    console.log("Going back to the previous page...");
+    // Go back to the previous page
+    window.history.back();
+  };
+
   return (
     <div className="text-black">
-        {children}
+      {children}
 
-      <div className=" h-full md:w-64 flex-col fixed inset-y-0 z-50 md:flex hidden">
+      <div className="h-full md:w-64 flex-col fixed inset-y-0 z-50 md:flex hidden"></div>
 
+      <div className="md:ml-64">
+        {/* Back button */}
+        <button
+          onClick={handleBackClick}
+          className="p-2 bg-fuchsia-600 text-white rounded-md ml-96 mt-3 w-36"
+        >
+          Return
+        </button>
       </div>
-
-      <div className="md:ml-64  ">
-
-        </div>
-
-
     </div>
   );
 }
-export default layout;
+
+export default Layout;
