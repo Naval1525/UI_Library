2278b5dc9d759a42ded4e93586001e0a2460cb22
STR7ANGER <adityamaurya.2807@gmail.com>
2024-11-22 01:11:03 +0530

    coming soon

--- app/(Dashboard)/(route)/radiogroup/page.js
+++ app/(Dashboard)/(route)/radiogroup/page.js
@@ -2,6 +2,12 @@ import Radiocom from "./Radiocom";
 import RadioShowcase from "./Radiocom";
 
 function RadioGroup() {
-  return <div><Radiocom></Radiocom></div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen bg-black">
+        <h1 className="text-white text-4xl font-bold">Coming Soon...</h1>
+      </div>
+    </div>
+  );
 }
 export default RadioGroup;
