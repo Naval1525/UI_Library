bd02e8836f5eb7231b57682a0b070e0c5ed44156
STR7ANGER <adityamaurya.2807@gmail.com>
2024-11-22 01:11:53 +0530

    coming soon

--- app/(Dashboard)/(route)/authform/page.js
+++ app/(Dashboard)/(route)/authform/page.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import UILibraryShowcase from './UILibraryShowcase'
+import React from "react";
+import UILibraryShowcase from "./UILibraryShowcase";
 
 function authform() {
   return (
-    <div><UILibraryShowcase></UILibraryShowcase></div>
-  )
+    <div>
+      <div className="flex items-center justify-center min-h-screen bg-black">
+        <h1 className="text-white text-4xl font-bold">Coming Soon...</h1>
+      </div>
+    </div>
+  );
 }
 
-export default authform
+export default authform;
